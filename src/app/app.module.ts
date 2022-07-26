b9bd5ee6ced1b863b7c5ab3660451ae1e2a88704
same-hack <samehack.info@gmail.com>
2022-07-26 11:50:56 +0900

    ルーティングテスト

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ConverterComponent } from './converter/converter.component';
 import { HeaderComponent } from './header/header.component';
-import { RoutingModule } from './routing.module';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent, ConverterComponent, HeaderComponent],
-  // imports: [BrowserModule, AppRoutingModule, FormsModule, RoutingModule],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
