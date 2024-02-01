979337cd7e13f427027ea6e4cd8bb090fb10a178
same-hack <samehack.info@gmail.com>
2024-02-01 15:44:11 +0900

    インサート文生成ツール

--- src/app/sql-generator/sql-generator.component.ts
+++ src/app/sql-generator/sql-generator.component.ts
@@ -9,13 +9,17 @@ interface SelectBoxOption {
   styleUrls: ['../common/common.scss', './sql-generator.component.scss'],
 })
 export class SqlGeneratorComponent {
+  // 初期スキーマとテーブル
   schema = 'スキーマ';
   table = 'テーブル';
 
+  // デフォルトのデータオブジェクト
   datas: any = [{ column: 'カラム', value: "'データ'", isIncrement: false }];
 
+  // 追加するデータの数
   dataCount = 1;
 
+  // カラムを追加するメソッド
   addColumn() {
     this.datas.push({
       column: 'カラム',
@@ -24,11 +28,37 @@ export class SqlGeneratorComponent {
     });
   }
 
+  // カラムを削除するメソッド
   dropColumn() {
     this.datas.pop();
   }
 
+  // データの数に基づいて配列を生成するメソッド
   calcCount(dataCount: number) {
     return [...Array(dataCount)];
+  }
+
+  // 生成されたSQLコードをクリップボードにコピーするメソッド
+  getCode() {
+    // 特定のdiv要素を取得
+    const element = document.getElementById('code');
+
+    // div内のすべてのテキストを取得
+    const allText: string =
+      String(element?.textContent) || String(element?.innerText);
+
+    // 新しいテキストエリアを作成し、その中にテキストを設定
+    const textArea = document.createElement('textarea');
+    textArea.value = allText;
+
+    // テキストエリアをDOMに追加
+    document.body.appendChild(textArea);
+
+    // テキストエリアを選択してコピー
+    textArea.select();
+    document.execCommand('copy');
+
+    // テキストエリアを削除
+    document.body.removeChild(textArea);
   }
 }
